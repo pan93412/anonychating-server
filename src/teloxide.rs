5b5f96bcaaafdd8e57c57960d9cedeb1cc171f97
pan93412 <pan93412@gmail.com>
2020-08-28 20:48:38 +0800

    teloxide: partially reverted d8a0d7f
    it is too annoying and eats
    too many meaningless resources!

--- src/teloxide.rs
+++ src/teloxide.rs
@@ -4,12 +4,12 @@ use crate::{config::Config, CONFIG_FILENAME, log::log_info};
 pub async fn create_bot() -> teloxide::Bot {
     let config = Config::from_file(CONFIG_FILENAME);
     let bot = BotBuilder::new().token(config.telexide.bot_token).build();
-    let me = bot.get_me().send().await.expect("The bot token should be invaild.");
-    log_info("Telexide.CreateBot", &format!(
-        "Created a bot instance to @{}. It forwards messages to: {}",
-        me.user.username.expect("Bots should have username"),
-        config.telexide.publish_channel,
-    ));
+    // let me = bot.get_me().send().await.expect("The bot token should be invaild.");
+    // log_info("Telexide.CreateBot", &format!(
+    //     "Created a bot instance to @{}. It forwards messages to: {}",
+    //     me.user.username.expect("Bots should have username"),
+    //     config.telexide.publish_channel,
+    // ));
 
     bot
 }
