12fbbe56ab8b2f2a808f3ea27cca51e360fcee5e
pan93412 <pan93412@gmail.com>
2020-08-28 09:01:18 +0800

    feat(teloxide): it is not a dice bot!

--- src/teloxide.rs
+++ src/teloxide.rs
@@ -2,8 +2,6 @@ use teloxide::prelude::*;
 
 pub fn create_bot() -> teloxide::Bot {
     teloxide::enable_logging!();
-    log::info!("Starting dices_bot...");
-
     let bot = Bot::from_env();
     
     bot
