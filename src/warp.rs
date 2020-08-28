a7efe3a2f15aec2829b1cca00178e42923fa5d99
pan93412 <pan93412@gmail.com>
2020-08-28 20:42:53 +0800

    warp: show the info for warp

--- src/warp.rs
+++ src/warp.rs
@@ -1,6 +1,6 @@
 use warp::{Filter, reject::Reject, Rejection, Reply};
 use serde::Deserialize;
-use crate::{publish, teloxide::create_bot, config::Config, CONFIG_FILENAME};
+use crate::{publish, teloxide::create_bot, config::Config, CONFIG_FILENAME, log::log_info};
 
 #[derive(Deserialize)]
 /// The request to /publish.
@@ -24,13 +24,14 @@ async fn error_handler(_rejection: Rejection) -> Result<impl Reply, std::convert
 }
 
 pub async fn warp_server() {
+    let wc = Config::from_file(CONFIG_FILENAME).warp;
     let publish = warp::path!("publish")
         .and(warp::query::<PublishRequest>())
         .and_then(|query: PublishRequest| async move {
             let config = Config::from_file(CONFIG_FILENAME);
 
             let request = publish::publish(
-                &create_bot(),
+                &create_bot().await,
                 publish::MessageRequest::new(query.message),
                 config.telexide.publish_channel,
             ).await;
@@ -42,6 +43,19 @@ pub async fn warp_server() {
             }
         }).recover(error_handler);
 
-    let wc = Config::from_file(CONFIG_FILENAME).warp;
+    // wc.server_ip
+    //  1. 先變成迭代器，以便 map 成字串: iter()
+    //  2. 將每個內部的數字變成字串: map(|v| v.to_string())
+    //  3. 然後，重新組回一個 Vector (顯式宣告): .collect::<Vec<String>>()
+    //  4. 最後，中間加點點: .join(".")
+    log_info("Warp.Main", &format!(
+        "Server is running on: {}:{}",
+        wc.server_ip
+            .iter()
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>()
+            .join("."),
+        wc.server_port
+    ));
     warp::serve(publish).run((wc.server_ip, wc.server_port)).await;
 }
