d8a0d7f54f34c003421bc12eb11d46d8dd91aaed
pan93412 <pan93412@gmail.com>
2020-08-28 20:42:33 +0800

    teloxide: show the info message for teloxide

--- src/teloxide.rs
+++ src/teloxide.rs
@@ -1,7 +1,15 @@
-use teloxide::{BotBuilder};
-use crate::{config::Config, CONFIG_FILENAME};
+use teloxide::{BotBuilder, prelude::Request};
+use crate::{config::Config, CONFIG_FILENAME, log::log_info};
 
-pub fn create_bot() -> teloxide::Bot {
+pub async fn create_bot() -> teloxide::Bot {
     let config = Config::from_file(CONFIG_FILENAME);
-    BotBuilder::new().token(config.telexide.bot_token).build()
+    let bot = BotBuilder::new().token(config.telexide.bot_token).build();
+    let me = bot.get_me().send().await.expect("The bot token should be invaild.");
+    log_info("Telexide.CreateBot", &format!(
+        "Created a bot instance to @{}. It forwards messages to: {}",
+        me.user.username.expect("Bots should have username"),
+        config.telexide.publish_channel,
+    ));
+
+    bot
 }
